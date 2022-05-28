2de01eca5ef93b73fdb17b7fc7831d4cad603c86
Kirill <104451159+Modestra@users.noreply.github.com>
2022-05-28 20:59:28 +0700

    28.05.2022

--- ChemicalApp/ChemicalApp/Form1.cs
+++ ChemicalApp/ChemicalApp/Form1.cs
@@ -7,8 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using DebugUtilites;
 using ChemicalFormulas;
+using DebugUtilites;
 
 namespace ChemicalApp
 {
--- ChemicalApp/ChemicalFormulas/ControllerFormulas.cs
+++ ChemicalApp/ChemicalFormulas/ControllerFormulas.cs
@@ -2,7 +2,49 @@
 
 namespace ChemicalFormulas
 {
-    public class Class1
+    public class ControllerFormulas
     {
+        public void NumberRule(int val)
+        {
+            if (val < 0 || val > 14)
+            {
+                throw new NumberException("", val);
+
+            }
+        }
     }
+    public abstract class OxidIndex
+    { //pH
+        public abstract void IndexHydrogen();
+    }
+    public class OxidSub : OxidIndex
+    {
+        public override void IndexHydrogen()
+        {
+
+        }
+    }
+    public class BaseSub : OxidIndex
+    {
+        public override void IndexHydrogen()
+        {
+
+        }
+    }
+    public class NeitralSub : OxidIndex
+    {
+        public override void IndexHydrogen()
+        {
+
+        }
+    }
+    public class NumberException : Exception
+    {
+        public int Number { get; }
+        public NumberException(string message, int num) : base(message)
+        {
+            Number = num;
+        }
+    }
+
 }
