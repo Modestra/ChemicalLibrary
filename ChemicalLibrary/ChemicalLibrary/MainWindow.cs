cbdb6deb59ff27d919ba6b8360959c3a8429629f
Kirill <104451159+Modestra@users.noreply.github.com>
2022-05-30 12:07:48 +0700

    Start Position

--- ChemicalLibrary/ChemicalLibrary/MainWindow.cs
+++ ChemicalLibrary/ChemicalLibrary/MainWindow.cs
@@ -10,12 +10,16 @@ using System.Windows.Forms;
 
 namespace ChemicalLibrary
 {
-    public partial class Form1 : Form
+    public partial class MainWindow : Form
     {
-        public Form1()
+        public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
