e38460c9b35882b4da9e5ea9ec98ee3f7a91070d
Kirill <104451159+Modestra@users.noreply.github.com>
2022-06-09 11:38:24 +0700

    09.06.2022

---
+++ ChemicalLibrary/DataBase/ChemElementList.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Вещество] NTEXT NOT NULL, 
+    [Название вещества] NTEXT NOT NULL
+)
---
+++ ChemicalLibrary/DataBase/SQLQuery1.sql
@@ -0,0 +1,5 @@
+CREATE TABLE dbo.SubstanceListMain
+(  
+	 Вещество ntext NOT NULL  
+    ,Название_вещества ntext NOT NULL  
+);  
