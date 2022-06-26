54e76de6000d93d8208b09c85b66cf48a5b60205
Kirill <104451159+Modestra@users.noreply.github.com>
2022-06-27 01:07:04 +0700

    27.06.2022

--- ChemicalLibrary/MathMainFormulas/MathMainFormulas/MathLibrary.cs
+++ ChemicalLibrary/MathMainFormulas/MathMainFormulas/MathLibrary.cs
@@ -1,9 +1,41 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace MathMainFormulas
 {
     public class MathLibrary
     {
-
+        public static long Fact(long n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Fact(n - 1);
+        }
     }
+    public class Probability
+    {
+        private void Prob(List<double> number)
+        {
+            foreach(int numberItem in number)
+            {
+                double Item = numberItem / number.Sum();
+                number.Remove(numberItem);
+                number.Add(Item);
+            }
+        }
+        public void Bernully()
+        {
+
+        }
+    }
+    public class Combinatorics 
+    { 
+        public int CombSelection(int n, int k)
+        {
+            return 1;
+        }
+    }
+
+
 }
