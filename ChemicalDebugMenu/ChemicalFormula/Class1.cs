773fbf50e154a15aac3bfcce6c6b80e68df23ad0
Kirill <104451159+Modestra@users.noreply.github.com>
2022-12-25 13:49:17 +0700

    Создание нового проекта

---
+++ ChemicalDebugMenu/ChemicalFormula/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChemicalFormula
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ ChemicalDebugMenu/LibraryTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ChemicalFormula;
+using System;
+
+namespace LibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
