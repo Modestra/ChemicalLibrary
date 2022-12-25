739027898778bf535e8ed5ded2db8469abc57f6d
Kirill <104451159+Modestra@users.noreply.github.com>
2022-12-25 19:26:37 +0700

    Подключение базы данных

--- ChemicalMenu/ChemicalMenu/DataBaseConnect.cs
+++ ChemicalMenu/ChemicalMenu/DataBaseConnect.cs
@@ -2,12 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.Sql;
+using System.Data;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace ChemicalMenu
 {
     internal class DataBaseConnect
     {
+        public SqlConnection chemDB = null;
+        public DataBaseConnect()
+        {
+            chemDB = new SqlConnection(ConfigurationManager.ConnectionStrings["ChemDB"].ConnectionString);
+            chemDB.Open();
+        }
+        public void SelectAllTable(string tablename, DataGridView table)
+        {
+            try
+            {
+                SqlCommand select = new SqlCommand($"SELECT * FROM {tablename}");
+                select.ExecuteNonQuery();
+            }
+            catch
+            {
+                MessageBox.Show("Неизвестное название таблицы или неизвестное DataGridView");
+            }
+        }
     }
 }
