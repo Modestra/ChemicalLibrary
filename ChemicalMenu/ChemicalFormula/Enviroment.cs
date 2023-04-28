c3c2631c048b6be0d78475dfb74618f132670575
Kirill <104451159+Modestra@users.noreply.github.com>
2023-04-28 09:04:22 +0700

    Добавление проекта

--- ChemicalMenu/ChemicalFormula/Enviroment.cs
+++ ChemicalMenu/ChemicalFormula/Enviroment.cs
@@ -6,7 +6,6 @@ using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using ChemicalFormula;
 
 namespace EnviromentCore
 {
--- ChemicalMenu/ChemicalFormula/Molecula.cs
+++ ChemicalMenu/ChemicalFormula/Molecula.cs
@@ -10,7 +10,7 @@ using System.Collections;
 using Newtonsoft.Json;
 using System.Threading;
 
-namespace ChemicalFormula
+namespace EnviromentCore
 {
     [Serializable]
     public class Molecula
